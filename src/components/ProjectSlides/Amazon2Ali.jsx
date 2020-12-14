6fcd1e3a136d062e23ed9a8771a1e2879d1a9568
Jhappy77 <joel.happ@shaw.ca>
2020-12-14 11:51:21 -0700

    Fix video on mobile

--- src/components/ProjectSlides/Amazon2Ali.jsx
+++ src/components/ProjectSlides/Amazon2Ali.jsx
@@ -10,7 +10,7 @@ import shibe from '../../images/shibe.jpg';
 function Amazon2Ali(){
     return <div>
     <h3>CheaperInChina</h3>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/MsUlgAihHP8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe className="amazon2ali__video" src="https://www.youtube.com/embed/MsUlgAihHP8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     <p>
         CheaperInChina is a chrome extension that quickly searches for Amazon products on AliExpress to find better deals. Check out the video for more details, and message me if you'd like to check out the code!
     </p>
