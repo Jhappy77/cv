59c8e2af5fc1a1c0f233d4b61caa35a30b26e7c9
Jhappy77 <joel.happ@shaw.ca>
2020-12-20 16:44:58 -0700

    Updates

---
+++ precache-manifest.00b910f70932cf649491a2c89d3c59ce.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f43bed61d8b64bb3379e366fec59d71",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "66a24e3950050b206a71",
+    "url": "/cv/static/css/2.edaccb72.chunk.css"
+  },
+  {
+    "revision": "5226972c88aa53f82697",
+    "url": "/cv/static/css/main.c59bf4fb.chunk.css"
+  },
+  {
+    "revision": "66a24e3950050b206a71",
+    "url": "/cv/static/js/2.1520c4ff.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cv/static/js/2.1520c4ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5226972c88aa53f82697",
+    "url": "/cv/static/js/main.5a8597d8.chunk.js"
+  },
+  {
+    "revision": "7ec79cb9283d2cdce47e",
+    "url": "/cv/static/js/runtime-main.b5201a95.js"
+  },
+  {
+    "revision": "e7cb75cb49b903d067bf9b19e5946ea5",
+    "url": "/cv/static/media/courseregister.e7cb75cb.png"
+  },
+  {
+    "revision": "004f0224b33539947bd56b925b40ad70",
+    "url": "/cv/static/media/joelcircular.004f0224.jpg"
+  },
+  {
+    "revision": "a026f0a3b7ad5ba78636ae4371a730d3",
+    "url": "/cv/static/media/k4n_antwerp.a026f0a3.png"
+  },
+  {
+    "revision": "bafa2ee54c10ac7900b3ad44314c79aa",
+    "url": "/cv/static/media/k4n_ui.bafa2ee5.png"
+  },
+  {
+    "revision": "edf8453b4f734d46ae666dbd9b721b97",
+    "url": "/cv/static/media/montblanc.edf8453b.jpg"
+  },
+  {
+    "revision": "563dbfc47bc272e716daf733d8ae2350",
+    "url": "/cv/static/media/mv_mummified_creeper.563dbfc4.png"
+  },
+  {
+    "revision": "65a9f90dd331a503974267814f42c341",
+    "url": "/cv/static/media/mv_rat.65a9f90d.png"
+  },
+  {
+    "revision": "c234b67460c0dc601047332a6b3c9747",
+    "url": "/cv/static/media/mv_vamp.c234b674.png"
+  },
+  {
+    "revision": "90317f1045508517c1ca26aa871f7e0c",
+    "url": "/cv/static/media/shibe.90317f10.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/cv/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/cv/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/cv/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/cv/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "4acbe147901129148f0db01591063b8f",
+    "url": "/cv/static/media/wizwars1.4acbe147.png"
+  },
+  {
+    "revision": "55697bbac4a6d55ea6369f0c0640f856",
+    "url": "/cv/static/media/wizwars2.55697bba.png"
+  }
+]);
